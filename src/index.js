a51f039b7d9c550e5cba792767dad0428aab391f
Cringle4519 <allthingsarepossible99@yahoo.com>
2025-08-24 17:04:38 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
-            
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
